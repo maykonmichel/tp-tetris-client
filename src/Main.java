9ca38ae0347d085976f95aa77c82677300392738
Maykon deepin <maykonmichelpalma@gmail.com>
2019-06-17 15:28:18 -0300

    Added main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
-
-    public static Window player1, player2;
     public static void main(String[] args) {
-        player1 = new Window(1);
-        player2 = new Window(2);
+        new Menu();
     }
 }
